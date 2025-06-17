b53c7825ca8456140d28e5f97d1ed6164a28aa86
jpezuela <jorge.pezuela@hotmail.com>
2025-06-17 08:33:18 +0200

    cv: fix domain name

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -10,7 +10,7 @@ export const RESUME_DATA = {
 	avatar: '/assets/avatar.jpg',
 	personalWebsiteUrl: {
 		name: 'Portfolio',
-		url: 'https://personal-portfolio-7wiyhcccv-jorgepezuelas-projects.vercel.app'
+		url: 'https://jorgepezuela.site'
 	},
 	contact: {
 		link: {
